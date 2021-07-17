7cf0e5f175b19686b904bf96b60fbd299cebf315
redsti-github <82223195+redsti-github@users.noreply.github.com>
2021-07-17 18:50:25 +0200

    updated drill and added descriptions
    -plasma drill now breaks trees
    -log descriptions
    -sticky wheel inventory description

--- Survival/Scripts/game/interactable/PlasmaDrill.lua
+++ Survival/Scripts/game/interactable/PlasmaDrill.lua
@@ -8,8 +8,6 @@ function PlasmaDrill.server_onFixedUpdate( self, deltaTime )
 
 	local valid, result = sm.physics.raycast(self.shape:getWorldPosition(), self.shape:getWorldPosition() + (self.shape:getAt() * 5))
 
-	--print(result)
-
 
 	if (result.type == "harvestable" and valid) then
 		local Harvestable = result:getHarvestable()
@@ -20,9 +18,106 @@ function PlasmaDrill.server_onFixedUpdate( self, deltaTime )
 	if (result.type == "body" and valid) then
 		local shape = result:getShape()
 
-		if (shape:getShapeUuid() == obj_harvest_stone or shape:getShapeUuid() == obj_harvests_stones_p01 or shape:getShapeUuid() == obj_harvests_stones_p02 or shape:getShapeUuid() == obj_harvests_stones_p03 or shape:getShapeUuid() == obj_harvests_stones_p04 or shape:getShapeUuid() == obj_harvests_stones_p05 or shape:getShapeUuid() == obj_harvests_stones_p06 or shape:getShapeUuid() == obj_harvest_stonechunk01 or shape:getShapeUuid() == obj_harvest_stonechunk02 or shape:getShapeUuid() == obj_harvest_stonechunk03) then
+		if (IsTree(shape:getShapeUuid()) or shape:getShapeUuid() == obj_harvest_stone or shape:getShapeUuid() == obj_harvests_stones_p01 or shape:getShapeUuid() == obj_harvests_stones_p02 or shape:getShapeUuid() == obj_harvests_stones_p03 or shape:getShapeUuid() == obj_harvests_stones_p04 or shape:getShapeUuid() == obj_harvests_stones_p05 or shape:getShapeUuid() == obj_harvests_stones_p06 or shape:getShapeUuid() == obj_harvest_stonechunk01 or shape:getShapeUuid() == obj_harvest_stonechunk02 or shape:getShapeUuid() == obj_harvest_stonechunk03) then
 
 			shape:getInteractable():setPublicData({1, result.pointWorld})
 		end
 	end
+end
+
+function IsTree( Uuid )
+	
+
+	if obj_harvest_log_s01 == Uuid then return true end
+	if obj_harvest_log_m01 == Uuid then return true end
+	if obj_harvest_log_l01 == Uuid then return true end
+	if obj_harvest_log_l02a == Uuid then return true end
+	if obj_harvest_log_l02b == Uuid then return true end
+	if obj_harvests_trees_spruce02_p00 == Uuid then return true end
+	if obj_harvests_trees_spruce02_p01 == Uuid then return true end
+	if obj_harvests_trees_spruce02_p02 == Uuid then return true end
+	if obj_harvests_trees_spruce02_p03 == Uuid then return true end
+	if obj_harvests_trees_spruce02_p04 == Uuid then return true end
+	if obj_harvests_trees_spruce01_p05 == Uuid then return true end
+	if obj_harvests_trees_spruce02_p05 == Uuid then return true end
+	if obj_harvests_trees_spruce03_p05 == Uuid then return true end
+	if obj_harvests_trees_leafy01_p00 == Uuid then return true end
+	if obj_harvests_trees_leafy01_p01 == Uuid then return true end
+	if obj_harvests_trees_leafy01_p02 == Uuid then return true end
+	if obj_harvests_trees_leafy01_p03 == Uuid then return true end
+	if obj_harvests_trees_leafy01_p04 == Uuid then return true end
+	if obj_harvests_trees_leafy02_p00 == Uuid then return true end
+	if obj_harvests_trees_leafy02_p01 == Uuid then return true end
+	if obj_harvests_trees_leafy02_p02 == Uuid then return true end
+	if obj_harvests_trees_leafy02_p03 == Uuid then return true end
+	if obj_harvests_trees_leafy02_p04 == Uuid then return true end
+	if obj_harvests_trees_leafy02_p05 == Uuid then return true end
+	if obj_harvests_trees_leafy02_p06 == Uuid then return true end
+	if obj_harvests_trees_leafy02_p07 == Uuid then return true end
+	if obj_harvests_trees_leafy03_p00 == Uuid then return true end
+	if obj_harvests_trees_leafy03_p01 == Uuid then return true end
+	if obj_harvests_trees_leafy03_p02 == Uuid then return true end
+	if obj_harvests_trees_leafy03_p03 == Uuid then return true end
+	if obj_harvests_trees_leafy03_p04 == Uuid then return true end
+	if obj_harvests_trees_leafy03_p05 == Uuid then return true end
+	if obj_harvests_trees_leafy03_p06 == Uuid then return true end
+	if obj_harvests_trees_leafy03_p07 == Uuid then return true end
+	if obj_harvests_trees_leafy03_p08 == Uuid then return true end
+	if obj_harvests_trees_leafy03_p09 == Uuid then return true end
+	if obj_harvests_trees_birch01_p00 == Uuid then return true end
+	if obj_harvests_trees_birch01_p01 == Uuid then return true end
+	if obj_harvests_trees_birch01_p02 == Uuid then return true end
+	if obj_harvests_trees_birch01_p03 == Uuid then return true end
+	if obj_harvests_trees_birch01_p04 == Uuid then return true end
+	if obj_harvests_trees_birch01_p05 == Uuid then return true end
+	if obj_harvests_trees_birch02_p00 == Uuid then return true end
+	if obj_harvests_trees_birch02_p01 == Uuid then return true end
+	if obj_harvests_trees_birch02_p02 == Uuid then return true end
+	if obj_harvests_trees_birch02_p03 == Uuid then return true end
+	if obj_harvests_trees_birch02_p04 == Uuid then return true end
+	if obj_harvests_trees_birch02_p05 == Uuid then return true end
+	if obj_harvests_trees_birch02_p06 == Uuid then return true end
+	if obj_harvests_trees_birch03_p00 == Uuid then return true end
+	if obj_harvests_trees_birch03_p01 == Uuid then return true end
+	if obj_harvests_trees_birch03_p02 == Uuid then return true end
+	if obj_harvests_trees_birch03_p03 == Uuid then return true end
+	if obj_harvests_trees_birch03_p04 == Uuid then return true end
+	if obj_harvests_trees_birch03_p05 == Uuid then return true end
+	if obj_harvests_trees_birch03_p06 == Uuid then return true end
+	if obj_harvests_trees_pine01_p00 == Uuid then return true end
+	if obj_harvests_trees_pine01_p01 == Uuid then return true end
+	if obj_harvests_trees_pine01_p02 == Uuid then return true end
+	if obj_harvests_trees_pine01_p03 == Uuid then return true end
+	if obj_harvests_trees_pine01_p04 == Uuid then return true end
+	if obj_harvests_trees_pine01_p05 == Uuid then return true end
+	if obj_harvests_trees_pine01_p06 == Uuid then return true end
+	if obj_harvests_trees_pine01_p07 == Uuid then return true end
+	if obj_harvests_trees_pine01_p08 == Uuid then return true end
+	if obj_harvests_trees_pine01_p09 == Uuid then return true end
+	if obj_harvests_trees_pine01_p10 == Uuid then return true end
+	if obj_harvests_trees_pine01_p11 == Uuid then return true end
+	if obj_harvests_trees_pine02_p00 == Uuid then return true end
+	if obj_harvests_trees_pine02_p01 == Uuid then return true end
+	if obj_harvests_trees_pine02_p02 == Uuid then return true end
+	if obj_harvests_trees_pine02_p03 == Uuid then return true end
+	if obj_harvests_trees_pine02_p04 == Uuid then return true end
+	if obj_harvests_trees_pine02_p05 == Uuid then return true end
+	if obj_harvests_trees_pine02_p06 == Uuid then return true end
+	if obj_harvests_trees_pine02_p07 == Uuid then return true end
+	if obj_harvests_trees_pine02_p08 == Uuid then return true end
+	if obj_harvests_trees_pine02_p09 == Uuid then return true end
+	if obj_harvests_trees_pine02_p10 == Uuid then return true end
+	if obj_harvests_trees_pine03_p00 == Uuid then return true end
+	if obj_harvests_trees_pine03_p01 == Uuid then return true end
+	if obj_harvests_trees_pine03_p02 == Uuid then return true end
+	if obj_harvests_trees_pine03_p03 == Uuid then return true end
+	if obj_harvests_trees_pine03_p04 == Uuid then return true end
+	if obj_harvests_trees_pine03_p05 == Uuid then return true end
+	if obj_harvests_trees_pine03_p06 == Uuid then return true end
+	if obj_harvests_trees_pine03_p07 == Uuid then return true end
+	if obj_harvests_trees_pine03_p08 == Uuid then return true end
+	if obj_harvests_trees_pine03_p09 == Uuid then return true end
+	if obj_harvests_trees_pine03_p10 == Uuid then return true end
+	return false
+
 end
